598bd1ace77aed3285f312c44b30df95f90bae2a
jinbaoziyl <185284033@qq.com>
2020-02-22 07:57:18 +0800

    1.增加 支持经典卷积神经网络LeNet模型， 支持cpu或者Gpu-Cuda训练

---
+++ src/CNN/LeNet.py
@@ -0,0 +1,47 @@
+# 卷积神经网络- 含卷积层的网络
+# 模型设计: LeNet交替使用卷积层和最大池化层(2个)后接3个全连接层处理图像分类
+# 意义: 一方面，卷积层保留输入形状，使图像的像素在高和宽两个方向上的相关性均可能被有效识别
+#       另一方面，卷积层通过滑动窗口将同一卷积核与不同位置的输入重复计算，从而避免参数尺寸过大
+import torch
+import time
+from torch import nn, optim
+import sys
+sys.path.append("D:/NN/OpenDL/src/")
+import open_dl_utils as d2l
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#device = torch.device('cpu')
+
+# 定义LeNet模型
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet,self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1,6,5),   # no padding -> 28 - 5 + 1 = 24
+            nn.Sigmoid(),
+            nn.MaxPool2d(2,2),  # 24/2 = 12
+            nn.Conv2d(6,16,5),  # no padding -> 12 - 5 + 1 = 8  尺寸减少，增加输出通道使两个卷积层的参数尺寸类似
+            nn.Sigmoid(),
+            nn.MaxPool2d(2,2),  # 8/2 = 4  ==> 16channes  h,w = 4,4
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(16*4*4, 120),
+            nn.Sigmoid(),
+            nn.Linear(120, 84),
+            nn.Sigmoid(),
+            nn.Linear(84, 10),
+            nn.Sigmoid(),            
+        )
+
+    def forward(self, img):
+        feature = self.conv(img)
+        out = self.fc(feature.view(img.shape[0], -1))
+        return out 
+
+if __name__ == '__main__':
+    lr, num_epochs, batch_size = 0.001, 5, 256
+    train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size)
+
+    net = LeNet()
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    d2l.train_cnn(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs)
+
--- src/open_dl_utils/utils.py
+++ src/open_dl_utils/utils.py
@@ -84,12 +84,15 @@ def load_data_fashion_mnist(batch_size, resize=None, root='~/Datasets/FashionMNI
     return train_iter, test_iter
 
 
-def evaluate_accuracy(data_iter, net):
+def evaluate_accuracy(data_iter, net, device=None):
+    if device is None and isinstance(net, torch.nn.Module):
+        # 如果没指定device就使用net的device
+        device = list(net.parameters())[0].device
     acc_sum, n = 0.0, 0
     for X, y in data_iter:
         if isinstance(net, torch.nn.Module): # 继承pytorch模型
             net.eval() # 评估模式, 这会关闭dropout
-            acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+            acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu().item()
             net.train() # 改回训练模式
         else: # 自定义的模型
             if('is_training' in net.__code__.co_varnames): # 在net中存在is_training函数参数
@@ -145,4 +148,27 @@ def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
     if x2_vals and y2_vals:
         plt.semilogy(x2_vals, y2_vals, linestyle=':')
         plt.legend(legend)
+
+
+def train_cnn(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs):
+    net = net.to(device)
+    print("training on ", device)
+    loss = torch.nn.CrossEntropyLoss()
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, batch_count, start = 0.0, 0.0, 0, 0, time.time()
+        for X, y in train_iter:
+            X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            train_l_sum += l.cpu().item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
+            n += y.shape[0]
+            batch_count += 1
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec'
+              % (epoch + 1, train_l_sum / batch_count, train_acc_sum / n, test_acc, time.time() - start))
 
